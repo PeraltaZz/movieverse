8adb2a07cb478bb5e22eda8bff166d312a6fe287
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-07-07 12:32:13 -0400

    feat:adição do Footer no Layout

--- src/components/commons/Layout.tsx
+++ src/components/commons/Layout.tsx
@@ -1,16 +1,17 @@
 import { ReactNode } from "react";
 import { Header } from "./Header";
+import Footer from "./Footer";
 
-interface LayoutProps{
-    children:ReactNode;
+interface LayoutProps {
+  children: ReactNode;
 }
 
-export const Layout =({children}:LayoutProps)=>{
-    return(
-        <>
-        <Header/>
-        {children}
-
-        </>
-    )
-}
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
