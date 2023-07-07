d7d4037ed369af7ec9be093f1ecbec48637786ed
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-07-07 09:02:08 -0400

    feat: organização e criação de pastas

---
+++ src/pages/favorite-pages.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+
+const FavoritesPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Favorites | MovieVerse</title>
+      </Head>
+      
+     
+    </>
+  );
+};
+
+export default FavoritesPage;
