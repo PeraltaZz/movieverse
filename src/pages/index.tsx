4a94ea9ce215902e421c90168949e5a5c068a35f
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-07-07 12:33:49 -0400

    feat: adição do Search e do Movie no index tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
-import Head from "next/head";
+import { useState } from "react";
+import Search from "@/components/home/Search";
+import Movie from "@/components/home/Movie";
 
 const Home = () => {
+  const [selectedMovieKey, setSelectedMovieKey] = useState("");
+  const [isMovieSelected, setIsMovieSelected] = useState(false);
+
+  const onMovieClick = (movieKey: string) => {
+    setSelectedMovieKey(movieKey);
+    setIsMovieSelected(true);
+  };
+
   return (
     <>
-      <Head>
-        <title>Home | MovieVerse</title>
-      </Head>
+      <div className="main-container">
+        <Search onMovieClick={onMovieClick} />
+        <div className="default-container">
+          {isMovieSelected ? (
+            <Movie movieKey={selectedMovieKey} />
+          ) : (
+            <div className=""></div>
+          )}
+        </div>
+      </div>
     </>
   );
 };
