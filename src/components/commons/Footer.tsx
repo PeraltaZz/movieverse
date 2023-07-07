3f54915c34439ed1716f4068556a498a2e78c404
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-07-07 11:55:56 -0400

    feat: adição do componente footer

---
+++ src/components/commons/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+    return (
+      <footer className="footer-bg d-flex">
+        <p className="text-m">©MovieVerse 2023</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
