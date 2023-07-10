2294b4a0d5b8db2bd65fa4a31213234d532913fa
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-07-10 13:05:01 -0400

    update: favicon adicionado

--- src/components/commons/Layout.tsx
+++ src/components/commons/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { Header } from "./Header";
 import Footer from "./Footer";
+import Head from "next/head";
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,6 +10,9 @@ interface LayoutProps {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg" />
+      </Head>
       <Header />
       {children}
       <Footer />
