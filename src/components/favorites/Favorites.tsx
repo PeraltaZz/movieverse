1f4fc18adc2795d348c008e9c66b9fed20b16d75
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-07-10 15:37:44 -0400

    update: melhorias no componente favorites

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -15,60 +15,57 @@ interface Movie {
 const Favorites: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
-  useEffect(() => {
-    const favorites = localStorage.getItem("favorites");
-    if (favorites) {
-      const parsedFavorites = JSON.parse(favorites);
-      const movieKeys = Object.keys(parsedFavorites);
-      const moviePromises = movieKeys.map((movieKey) =>
-        fetch(`https://www.omdbapi.com/?i=${movieKey}&apikey=5862fd8f`)
-          .then((response) => response.json())
-          .catch((error) => {
-            console.error(
-              `Error fetching movie data for ID ${movieKey}:`,
-              error
-            );
-            return null;
-          })
-      );
+  const fetchMoviesData = async (movieKeys: string[]) => {
+    const moviePromises = movieKeys.map(async (movieKey) => {
+      try {
+        const response = await fetch(
+          `https://www.omdbapi.com/?i=${movieKey}&apikey=5862fd8f`
+        );
 
-      Promise.all(moviePromises)
-        .then((moviesData) => {
-          const filteredMoviesData = moviesData.filter((data) => data !== null);
-          const formattedMovies = filteredMoviesData.map((data) => {
-            const {
-              Poster,
-              Title,
-              imdbRating,
-              Runtime,
-              Released,
-              Genre,
-              imdbID,
-            } = data;
-            return {
-              Poster,
-              Title,
-              imdbRating,
-              Runtime,
-              Released,
-              Genre,
-              imdbID,
-            };
-          });
-          setMovies(formattedMovies);
-        })
-        .catch((error) => {
-          console.error("Error fetching movie data:", error);
-        });
-    }
+        const movieData = await response.json();
+        const movie = {
+          Poster: movieData.Poster,
+          Title: movieData.Title,
+          imdbRating: movieData.imdbRating,
+          Runtime: movieData.Runtime,
+          Released: movieData.Released,
+          Genre: movieData.Genre,
+          imdbID: movieData.imdbID,
+        };
+        return movie;
+      } catch (error) {
+        console.error(`Erro ao buscar dados do filme com ID ${movieKey}:`);
+      }
+    });
+
+    const moviesData = await Promise.all(moviePromises);
+    return moviesData;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const favorites = localStorage.getItem("favorites");
+        if (favorites) {
+          const parsedFavorites = JSON.parse(favorites);
+          const movieKeys = Object.keys(parsedFavorites);
+          const moviesData = await fetchMoviesData(movieKeys);
+          setMovies(moviesData as Movie[]);
+        }
+      } catch (error) {
+        console.error("Error fetching movie data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
     <div className="favorite-bg d-flex column g-20">
-      {movies.map((movie, index) => (
+      {movies.map((movie) => (
         <div
           className="favorite-all container d-grid align-items-center g-40"
-          key={index}
+          key={movie.imdbID}
         >
           <div className="favorite-img">
             <Image
