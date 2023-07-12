c88df5d507e960d3d51b93464d361f16540729c4
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-07-11 20:02:53 -0400

    update: otimização da chamada do componente movie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,10 @@ import Head from "next/head";
 
 const Home = () => {
   const [selectedMovieKey, setSelectedMovieKey] = useState("");
-  const [isMovieSelected, setIsMovieSelected] = useState(false);
+
 
   const onMovieClick = (movieKey: string) => {
     setSelectedMovieKey(movieKey);
-    setIsMovieSelected(true);
   };
 
   return (
@@ -17,15 +16,10 @@ const Home = () => {
       <Head>
         <title>Home | MovieVerse</title>
       </Head>
+
       <div className="main-container">
         <Search onMovieClick={onMovieClick} />
-        <div className="default-container">
-          {isMovieSelected ? (
-            <Movie movieKey={selectedMovieKey} />
-          ) : (
-            <div className=""></div>
-          )}
-        </div>
+        <Movie movieKey={selectedMovieKey} />
       </div>
     </>
   );
