24ee4282a9e88509ce56fab728b16f347f22095b
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-07-12 10:16:12 -0400

    update: caminhos do svg

--- src/components/commons/FavoriteIcon.tsx
+++ src/components/commons/FavoriteIcon.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import Star from "../../../public/star.svg";
-import StarFill from "../../../public/starfill.svg";
+import Star from 'public/star.svg';
+import StarFill from 'public/starfill.svg';
 
 interface FavoriteIconProps {
   movieKey: string;
